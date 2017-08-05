a48bcc5755ad05f4d1128c5c11f97f2f41329d3d
EderSolorzano <edersolorzano2013@gmail.com>
2017-08-04 22:57:46 -0500

    Completo-arreglando detalles

--- Natura Ayacucho/Controllers/CompraController.cs
+++ Natura Ayacucho/Controllers/CompraController.cs
@@ -8,10 +8,12 @@ namespace Natura_Ayacucho.Controllers
 {
     public class CompraController : Controller
     {
+        private Models.TiendaEntities1 bd = new Models.TiendaEntities1();
         // GET: Compra
         public ActionResult Paso1()
         {
-            return View();
+            var cliente = bd.Cliente.Find(Helper.SessionHelper.GetUser());
+            return View(cliente);
         }
 
         public ActionResult Paso2()
@@ -24,7 +26,45 @@ namespace Natura_Ayacucho.Controllers
             return View();
         }
 
-        public ActionResult Paso3()
+        [HttpPost]
+        public JsonResult RealizarPedido(List<Pedidos> p)
+        {
+            // guardar en base de datos
+            var clienteid = Helper.SessionHelper.GetUser();
+        var pcab = new Models.Pedido
+        {
+            ClienteId = clienteid,
+            Estado = "P",
+                Fecha = DateTime.Now
+    };
+
+    bd.Pedido.Add(pcab);
+            bd.SaveChanges();
+
+
+            foreach (var item in p)
+            {
+                var pdet = new Models.PedidoDetalle
+                {
+                    PedidoId = pcab.PedidoId,
+                    Cantidad = item.Cantidad,
+                    ProductoId = item.ProductoId
+                };
+    bd.PedidoDetalle.Add(pdet);
+                bd.SaveChanges();
+            }
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        public class Pedidos
+{
+    public int ProductoId { get; set; }
+    public string Denominacion { get; set; }
+    public int Cantidad { get; set; }
+    public string Imagen { get; set; }
+    public decimal Precio { get; set; }
+}
+public ActionResult Paso3()
         {
             return View();
         }
--- Natura Ayacucho/Controllers/HomeController.cs
+++ Natura Ayacucho/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace Natura_Ayacucho.Controllers
         {
             //logica de acceso para bd
             var productos = bd.Producto
-                .Where(x => x.Denominacion.Contains(id))// x.Descripcion.Contains(id))
+                .Where(x => x.Denominacion.Contains(id)  || x.Descripcion.Contains(id))// x.Descripcion.Contains(id))
                .Take(20)
                 .ToList();
             ViewBag.clave = id;
@@ -33,7 +33,7 @@ namespace Natura_Ayacucho.Controllers
             return View();
         }
 
-        public ActionResult Login()
+        public ActionResult Sesion()
         {
             return View();
         }
@@ -56,6 +56,46 @@ namespace Natura_Ayacucho.Controllers
         public ActionResult Resumen()
         {
             return View();
+        }
+
+        public ActionResult Productos(string id="")
+        {
+            var productos = bd.Producto
+                
+               .Take(50)
+                .ToList();
+            ViewBag.clave = id;
+            return View(productos);
+        }
+
+        public ActionResult Login(string usuario, string clave)
+        {
+            var u = bd.Cliente.FirstOrDefault(x => x.Usuario == usuario && x.Clave == clave);
+            if (u != null)
+            {
+                Helper.SessionHelper.AddUserToSession(u.ClienteId.ToString());
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Logout()
+        {
+            Helper.SessionHelper.DestroyUserSession();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult RegistrarCliente(Models.Cliente c)
+        {
+            bd.Cliente.Add(c);
+            bd.SaveChanges();
+            Helper.SessionHelper.AddUserToSession(c.ClienteId.ToString());
+            return RedirectToAction("Index", "Home");
+        }
+
+        public static string ObtenerNombreUsuario()
+        {
+            using (var b = new Models.TiendaEntities1())
+            {
+                return b.Cliente.Find(Helper.SessionHelper.GetUser()).Nombres;
+            }
         }
     }
 }
